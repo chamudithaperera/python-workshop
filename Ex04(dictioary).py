4f87b6aa621e44610d3b95fb470350cb28a7e4e3
chamudithaperera <chamudithakavishan2001@gmail.com>
2024-03-19 21:53:59 +0530

    first commit

---
+++ Ex04(dictioary).py
@@ -0,0 +1,37 @@
+list1=["apple","banana","cherry"]
+print(list1)
+print(type(list1))
+
+for x in list1:
+    print (x)
+    
+print()
+
+list1[2]="watermelon"
+print(list1)
+
+print(len(list1))
+
+print(list1.pop())
+print(list1)
+list1.insert(1,"stawberry")
+print(list1)
+
+tuple1=("car","van","bike")
+print(tuple1)
+print(type(tuple1))
+list1.extend(tuple1)
+print(list1)
+
+print()
+set1=('a','b','c','d')
+print(set1)
+print(type(set1))
+
+print()
+set2=('a','b','c','d','e')
+print(set2)
+set2.add('e')
+print(set2)
+
+di1=("number":2, "brand":"sofa")
